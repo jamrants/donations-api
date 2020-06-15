f4c50c44f68e99041ae93373fd84ac873f265b32
James Ah Yong <james@retrocraft.ca>
2020-06-15 18:13:17 -0400

    income function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const cors = require("cors")({origin: "https://donations.exposed"});
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: 'https://donations.exposed' });
 
 admin.initializeApp();
 let db = admin.firestore();
@@ -9,7 +9,7 @@ exports.donate = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     let donationId = req.body.id;
     let donationAmount = req.body.amount;
-    let donationRef = db.collection("donations").doc(donationId);
+    let donationRef = db.collection('donations').doc(donationId);
     donationRef
       .get()
       .then((docSnapshot) => {
@@ -28,5 +28,32 @@ exports.donate = functions.https.onRequest((req, res) => {
       .catch((err) => {
         res.status(500).json(err);
       });
+  });
+});
+
+exports.income = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    const country = req.query.country;
+    // implemented countries
+    if (!['CA', 'US'].includes(country)) {
+      res.status(404).json({ error: 'Country not supported.' });
+    } else {
+      // FSAs for Canada (first three digits of postal code)
+      const postcode = country === 'CA' ? req.query.postcode.substring(0, 3) : res.query.postcode;
+      db.collection(`postcode-income-${country.toLowerCase()}`)
+        .doc(postcode)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            res.status(200).json(doc.data());
+          } else {
+            res.status(404).json({ error: 'Postcode not found.' });
+          }
+          return;
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    }
   });
 });
