03710686b00a537d4dd6b5e70394b8fd4c0a2ad4
Jason Huang <jasonhuang20035@gmail.com>
2020-06-15 00:22:46 -0400

    still broken

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,32 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const cors = require("cors")({origin: "https://donations.exposed"});
 
 admin.initializeApp();
 let db = admin.firestore();
 
 exports.donate = functions.https.onRequest((req, res) => {
-  let donationId = req.body.id;
-  let donationAmount = req.body.amount;
-  let donationRef = db.collection("donations").doc(donationId);
-  donationRef.get().then((docSnapshot) => {
-    if (docSnapshot.exists) {
-      donationRef.update({
-        amount: admin.firestore.FieldValue.increment(donationAmount),
+  cors(req, res, () => {
+    let donationId = req.body.id;
+    let donationAmount = req.body.amount;
+    let donationRef = db.collection("donations").doc(donationId);
+    donationRef
+      .get()
+      .then((docSnapshot) => {
+        if (docSnapshot.exists) {
+          donationRef.update({
+            amount: admin.firestore.FieldValue.increment(donationAmount),
+          });
+        } else {
+          donationRef.set({
+            amount: donationAmount,
+          });
+        }
+        res.status(200).end();
+        return;
+      })
+      .catch((err) => {
+        res.status(500).json(err);
       });
-    } else {
-      donationRef.set({
-        amount: donationAmount,
-      });
-    }
-    res.status(200).end();
-    return;
-  }).catch((err) => {
-    res.status(500).json(err);
   });
 });
